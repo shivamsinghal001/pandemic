ca6ae37a75b40294ec5a72db74f7cb367b244164
Shivam Singhal <shivamsinghal@Shivams-MacBook-Pro.local>
2022-12-04 18:48:21 -0800

    Cleaned up dependencies

--- python/pandemic_simulator/data/__init__.py
+++
@@ -1,6 +0,0 @@
-# Confidential, Copyright 2020, Sony Corporation of America, All rights reserved.
-# flake8: noqa
-
-from .interfaces import *
-from .h5_data_loader import *
-from .h5_data_saver import *
--- python/pandemic_simulator/data/interfaces/__init__.py
+++
@@ -1,5 +0,0 @@
-# Confidential, Copyright 2020, Sony Corporation of America, All rights reserved.
-# flake8: noqa
-
-from .data_saver import *
-from .data_loader import *
