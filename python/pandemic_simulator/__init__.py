4d2fe403ab858edc30e3e1b3ba089453ae0ff853
shivamsinghal001 <60418185+shivamsinghal001@users.noreply.github.com>
2022-12-04 18:58:00 -0800

    Removed imports that aren't needed

--- python/pandemic_simulator/__init__.py
+++ python/pandemic_simulator/__init__.py
@@ -1,12 +1,9 @@
 # Confidential, Copyright 2020, Sony Corporation of America, All rights reserved.
 # flake8: noqa
 
-from . import data
 from . import environment
 from . import script_helpers
-from . import viz
 from . import utils
-from . import callback
 from . import model
 
 env = environment
